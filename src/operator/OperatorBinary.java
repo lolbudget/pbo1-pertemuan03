3976b7f594d2ea4e5806d667b3028629523a8463
lolbudget <elevenqim@gmail.com>
2020-05-02 06:34:10 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assignment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Mustaqim Noor";
+    int jumlahSks = 24;
+    double indeksPrestasiKomulatif = 3.90;
+}
